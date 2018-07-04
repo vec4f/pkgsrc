504b5a7e88cd0ac1c380a5622052f5b918c0e456
Jonathan Perkin <jperkin@joyent.com>
2018-07-04 14:08:23 +0100

    rust: Fix stack clash and mio.

--- lang/rust/patches/patch-src_libstd_sys_unix_thread.rs
+++ lang/rust/patches/patch-src_libstd_sys_unix_thread.rs
@@ -1,7 +1,18 @@
 $NetBSD: patch-src_libstd_sys_unix_thread.rs,v 1.3 2018/06/25 13:31:11 ryoon Exp $
 
---- src/libstd/sys/unix/thread.rs.orig	2018-06-24 22:42:29.203295357 +0000
+Fix stack-clash on illumos.
+
+--- src/libstd/sys/unix/thread.rs.orig 2018-06-24 22:42:29.203295357 +0000
 +++ src/libstd/sys/unix/thread.rs
+@@ -309,7 +309,7 @@ pub mod guard {
+ 
+         let stackaddr = get_stack_start_aligned()?;
+ 
+-        if cfg!(target_os = "linux") {
++        if cfg!(any(target_os = "linux", target_os = "solaris")) {
+             // Linux doesn't allocate the whole stack right away, and
+             // the kernel has its own stack-guard mechanism to fault
+             // when growing too close to an existing mapping.  If we map
 @@ -326,13 +326,22 @@ pub mod guard {
              // Reallocate the last page of the stack.
              // This ensures SIGBUS will be raised on
@@ -27,3 +38,12 @@ $NetBSD: patch-src_libstd_sys_unix_thread.rs,v 1.3 2018/06/25 13:31:11 ryoon Exp
              let guardaddr = stackaddr as usize;
              let offset = if cfg!(target_os = "freebsd") {
                  2
+@@ -345,7 +354,7 @@ pub mod guard {
+     }
+ 
+     pub unsafe fn deinit() {
+-        if !cfg!(target_os = "linux") {
++        if cfg!(not(any(target_os = "linux", target_os = "solaris"))) {
+             if let Some(stackaddr) = get_stack_start_aligned() {
+                 // Remove the protection on the guard page.
+                 // FIXME: we cannot unmap the page, because when we mmap()
